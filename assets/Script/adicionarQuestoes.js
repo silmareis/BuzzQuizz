686c170a2c6f924c6e22c9f93235b13c246b6e83
Silmar Reis <silmar.reis@yahoo.com.br>
2020-09-24 16:06:00 -0500

    Layout das questoes com imagens e respostas gerado

---
+++ assets/Script/adicionarQuestoes.js
@@ -0,0 +1,8 @@
+function abreQuiz() {
+    var elementoLogin = document.querySelector(".listaQuizzes");
+    elementoLogin.style.display = "none";  
+      
+   var elementoLogin = document.querySelector(".interfaceQuizz");
+   elementoLogin.style.display = "block";
+}
+
