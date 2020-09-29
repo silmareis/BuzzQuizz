d2723637e1987edf659accf5b1fc4e39f308b94a
Vagner Pavani <vagner.alves.pavani@gmail.com>
2020-09-29 15:33:27 -0300

    preparando review

--- assets/Script/fimQuizz.js
+++
@@ -1,7 +0,0 @@
-function resultado () {
-    var elementoLogin = document.querySelector(".resultadoQuizz");
-    elementoLogin.style.display = "block";  
-      
-   var elementoLogin = document.querySelector(".interfaceQuizz");
-   elementoLogin.style.display = "none";
-}
